265d2c3158ea1ebf4c26d26f840d919a662f1189
gauravkumarkeysight <129050192+gauravkumarkeysight@users.noreply.github.com>
2023-03-27 23:14:40 +0530

    Update main.py
    class based api

--- main.py
+++ main.py
@@ -1,89 +1,80 @@
-# import requests
-# import base64
-# import json
-# from secrets import *
-
-# # Step 1 - Authorization 
-# url = "https://accounts.spotify.com/api/token"
-# headers = {}
-# data = {}
-
-# # Encode as Base64
-# message = f"{clientId}:{clientSecret}"
-# messageBytes = message.encode('ascii')
-# base64Bytes = base64.b64encode(messageBytes)
-# base64Message = base64Bytes.decode('ascii')
-
-
-# headers['Authorization'] = f"Basic {base64Message}"
-# data['grant_type'] = "client_credentials"
-
-# r = requests.post(url, headers=headers, data=data)
-
-# token = r.json()['access_token']
-
-# # Step 2 - Use Access Token to call playlist endpoint
-
-# playlistId = "myPlaylistId"
-# playlistUrl = f"https://api.spotify.com/v1/playlists/{playlistId}"
-# headers = {
-#     "Authorization": "Bearer " + token
-# }
-
-# res = requests.get(url=playlistUrl, headers=headers)
-
-# print(json.dumps(res.json(), indent=2))
-
-# ==================
-
 
 import requests
-from urllib.parse import urlencode
-import base64
-import webbrowser
+import logging
+import secret_env as secret
 
-client_id = "3014ba0ea7e240fa9d36d3e519a5d73f"
-client_secret = "90af86f50bd5409ab12d830673d3f2ea"
+# ========================================
 
-auth_headers = {
-    "client_id": client_id,
-    "response_type": "code",
-    "redirect_uri": "http://localhost:7777/callback",
-    "scope": "user-library-read"
-}
+class SpotifyAPI():
+    def __init__(self, url='https://accounts.spotify.com/api/token'):
+        print('Welcome to the Spotify API')
+        self.AUTH_URL=  url 
+        self.access_token = self.get_Access_Token()        
+        self.BASE_URL = 'https://api.spotify.com/v1/'
 
-webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
 
-# code = "insert your authorization code here"
+# POST
+    def get_Access_Token(self):
+        try:
+            auth_response= requests.post(self.AUTH_URL, {
+                'grant_type': 'client_credentials',
+                'client_id':secret.CLIENT_ID,
+                'client_secret': secret.CLIENT_SECRET,
+            })
+        except Exception as err:
+            print(err)
+            logging.info(f'[!Error]:', {err})
 
-# encoded_credentials = base64.b64encode(client_id.encode() + b':' + client_secret.encode()).decode("utf-8")
 
-# token_headers = {
-#     "Authorization": "Basic " + encoded_credentials,
-#     "Content-Type": "application/x-www-form-urlencoded"
-# }
+        # convert the response to JSON
+        auth_response_data = auth_response.json()
+        # print(auth_response_data)
+        # save the access token
+        access_token = auth_response_data['access_token']
 
-# token_data = {
-#     "grant_type": "authorization_code",
-#     "code": code,
-#     "redirect_uri": "http://localhost:7777/callback"
-# }
-# r = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers)
+        print(access_token)
+        return access_token
 
-# token = r.json()["access_token"]
 
-# print(token)
 
-# user_headers = {
-#     "Authorization": "Bearer " + token,
-#     "Content-Type": "application/json"
-# }
+    def headers(self):
+        headers = {
+            'Authorization': 'Bearer {token}'.format(token=self.access_token)
+        }
+        return headers
 
-# user_params = {
-#     "limit": 50
-# }
+# actual GET request with proper header
 
-# user_tracks_response = requests.get("https://api.spotify.com/v1/me/tracks", params=user_params, headers=user_headers)
+    def get_track_info(self, track_id='6y0igZArWVi6Iz0rj35c1Y'):
+        try:
+            r = requests.get(self.BASE_URL + 'audio-features/' + track_id, headers=self.headers())
+            r= r.json()
+            print(r)
+            return r
+        except Exception as err:
+            print(err)
+            logging.info(f'[!Error]:', {err})    
 
-# print(user_tracks_response.json())
+    def get_artist_info(self, artist_id='36QJpDe2go2KgaRleHCDTp'):
+        try:
+            r = requests.get(self.BASE_URL + 'artists/' + artist_id + '/albums', 
+                    headers=self.headers(), 
+                    params={'include_groups': 'album', 'limit': 50})
+            d = r.json()
+            # print(d)
 
+            for album in d['items']:
+                print(album['name'], ' --- ', album['release_date'])
+        except Exception as err:
+            print(err)
+            logging.info(f'[!Error]:', {err})
+        # return d
+
+    def main(self):
+        self.get_Access_Token()
+        self.get_track_info()
+        self.get_artist_info()
+        
+
+obj = SpotifyAPI()
+obj.main()
